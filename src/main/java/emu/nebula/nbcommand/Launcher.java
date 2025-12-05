4d3088d92cefe96d5126cf4cae6f7b693a26b37d
HongchengQ <121159914+HongchengQ@users.noreply.github.com>
2025-12-06 02:39:07 +0800

    chore(version): 更新应用版本号至 v1.3.0
    - 将版本号从 v1.2.2 更新到 v1.3.0

--- src/main/java/emu/nebula/nbcommand/Launcher.java
+++ src/main/java/emu/nebula/nbcommand/Launcher.java
@@ -3,7 +3,7 @@ package emu.nebula.nbcommand;
 import javafx.application.Application;
 
 public class Launcher {
-    public static String version = "v1.2.2";
+    public static String version = "v1.3.0";
     public static void main(String[] args) {
 
         System.setProperty("javafx.platform", "desktop");
