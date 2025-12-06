a2035c3f328d545eb4396f646919a191e1b90393
HongchengQ <121159914+HongchengQ@users.noreply.github.com>
2025-12-06 19:20:27 +0800

    chore(version): 更新应用版本号
    - 将版本号从 v1.4.0 更新至 v1.4.1

--- src/main/java/emu/nebula/nbcommand/Launcher.java
+++ src/main/java/emu/nebula/nbcommand/Launcher.java
@@ -3,7 +3,7 @@ package emu.nebula.nbcommand;
 import javafx.application.Application;
 
 public class Launcher {
-    public static String version = "v1.4.0";
+    public static String version = "v1.4.1";
     public static void main(String[] args) {
 
         System.setProperty("javafx.platform", "desktop");
